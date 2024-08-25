d0cc12acff3bf372cd145729cd3f1015e405605d
Surya prakash <suryaharsh279@gmail.com>
2024-08-25 19:27:27 +0530

    ..

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,12 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 app.use(express.json({limit:"20kb"}))
 app.use("/restro",router)
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://spicemaven-online-food-ordering.vercel.app')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+})
 app.use(bodyParser.json()); 
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on port ${process.env.PORT}`);
