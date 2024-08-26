ba45d8e747117a63f73886c4a6502d0ab0550407
Surya prakash <suryaharsh279@gmail.com>
2024-08-26 12:39:07 +0530

    ..

--- Server/src/Controllers/login.js
+++ Server/src/Controllers/login.js
@@ -36,8 +36,7 @@ export const loginUser = async (req, res) => {
       res.cookie('token', token, {
         httpOnly: false,  
         secure: process.env.NODE_ENV === 'production',  
-        sameSite: 'Lax', 
-        maxAge: 1000 * 60 * 60 * 24 
+       
     });
     
   
