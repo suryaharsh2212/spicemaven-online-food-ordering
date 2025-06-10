f665121b00724ebe81347781b5d6cace82ac8e55
Surya prakash <suryaharsh279@gmail.com>
2025-06-10 15:15:55 +0530

    chnage in logout and register

--- Server/src/Controllers/register.js
+++ Server/src/Controllers/register.js
@@ -6,35 +6,30 @@ import { generateToken } from "../Utilities/generateToken.js";
 
 export const registerUser = async (req, res) => {
   try {
-    const { name, email, phoneNo,password } = req.body;
-   
-    
-    
-
-    if (!name || !email || !phoneNo || !password ) {
-      return res.status(400).json({ message: 'All fields are required',error:true });
+    const { name, email, phoneNo, password } = req.body;
+    if (!name || !email || !phoneNo || !password) {
+      return res.status(400).json({ message: 'All fields are required', error: true });
     }
-
     const existingUseremail = await spiceUser.findOne({ email });
     if (existingUseremail) {
-      return res.status(409).json({ message: 'spiceUser with email already exists', error:true, flag:"email" });
+      return res.status(409).json({ message: 'spiceUser with email already exists', error: true, flag: "email" });
     }
     const existingUserPhone = await spiceUser.findOne({ phoneNo });
     if (existingUserPhone) {
-      return res.status(409).json({ message: 'spiceUser with Phone no already exists',error:true,flag:"phone" });
+      return res.status(409).json({ message: 'spiceUser with Phone no already exists', error: true, flag: "phone" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    
-    
-      const token = generateToken(user);
-  
-      user.token = token;
-      await user.save();
+
+
+    const token = generateToken(user);
+
+    user.token = token;
+    await user.save();
     const newUser = new spiceUser({
       name,
       email,
       phoneNo,
-      password:hashedPassword
+      password: hashedPassword
     });
 
 
@@ -42,7 +37,7 @@ export const registerUser = async (req, res) => {
 
     res.status(201).json({
       message: 'spiceUser registered successfully',
-      error:false,
+      error: false,
       user: {
         id: newUser._id,
         name: newUser.name,
@@ -55,6 +50,6 @@ export const registerUser = async (req, res) => {
     });
   } catch (error) {
     console.error('Error registering user:', error);
-    res.status(500).json({ message: 'Internal server error',error:true });
+    res.status(500).json({ message: 'Internal server error', error: true });
   }
 };
