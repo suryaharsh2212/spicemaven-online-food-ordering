fa59cf3c2912c4be019a7119b46365b89a453bca
Surya prakash <suryaharsh279@gmail.com>
2025-11-07 22:56:01 +0530

    update in email ui

--- Server/src/paymentSetup/confirmpayment.js
+++ Server/src/paymentSetup/confirmpayment.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 import { Order } from '../Database/Model/OrderModel.js';
+import { sendOrderConfirmationEmail } from '../Utilities/createMail.js';
 
 export const confirmRazorpayPayment = async (req, res) => {
-    const { paymentId, orderId } = req.body;
+    const { paymentId, orderId ,user} = req.body;
     console.log('Confirming Razorpay payment for order ID:', orderId, 'with payment ID:', paymentId);
-
     if (!paymentId || !orderId) {
         return res.status(400).json({ success: false, message: 'Payment ID and Order ID are required' });
     }
@@ -40,13 +40,19 @@ export const confirmRazorpayPayment = async (req, res) => {
             return res.status(404).json({ success: false, message: 'Order not found' });
         }
 
-      
+
         order.payment_id = payment.id;
         order.razorpay_order_id = payment.order_id || null;
         order.payment_status = payment_status;
 
-    
+
         const updatedOrder = await order.save();
+        await sendOrderConfirmationEmail(
+            user.email, 
+            orderId,
+            user.name,
+            '30-45 minutes'
+        );
 
         return res.status(200).json({
             success: true,
@@ -54,6 +60,7 @@ export const confirmRazorpayPayment = async (req, res) => {
             order: updatedOrder,
             payment,
         });
+
 
     } catch (error) {
         console.error('Error confirming payment:', error);
